140544561e84a1c65b548efe3c6e12e0922f506b
Hampus Olsen <hampus.olsen@outlook.com>
2020-11-16 19:48:32 +0100

    fix(subscribers): Solves sticky navigation bug

--- src/components/views/Profile/Profile.view.tsx
+++ src/components/views/Profile/Profile.view.tsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import RoutePaths from "../../../config/router.config";
+import { useTrailingSlug } from "../../../resources/util/hooks";
 import Fullscreen from "../../common/wrappers/Fullscreen";
-import ViewWrapper from "../../common/wrappers/ViewWrapper";
 import ProfileHeader from "./sub-components/Header.sub";
 import ProfileNavigation from "./sub-components/Navigation.sub";
 
+const validSlugs = [
+  RoutePaths.FOLLOWERS,
+  RoutePaths.FOLLOWING,
+  RoutePaths.EDIT
+];
+
 const ProfileView: React.FC = () => {
+  const slug = useTrailingSlug();
+
+  if (!validSlugs.some((s) => s === slug)) {
+    return <Navigate to={`${RoutePaths.CONTACTS}/${RoutePaths.FOLLOWERS}`} />;
+  }
+
   return (
-    <Fullscreen overflow>
-      <ViewWrapper center>
-        <ProfileHeader />
-        <ProfileNavigation />
-        <Outlet />
-      </ViewWrapper>
+    <Fullscreen overFlow>
+      <ProfileHeader />
+      <ProfileNavigation />
+      <Outlet />
     </Fullscreen>
   );
 };
